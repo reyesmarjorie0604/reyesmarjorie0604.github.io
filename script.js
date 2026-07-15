5b6f2d43409605ea5d683cd72216333c25096401
Marjorie Reyes <reyesmarjorie0604@gmail.com>
2026-07-15 20:00:27 +0800

    againr

--- script.js
+++ script.js
@@ -29,14 +29,6 @@ window.addEventListener("scroll", () => {
     });
 
 });
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
-
-menuToggle.addEventListener("click", () => {
-
-    navLinks.classList.toggle("active");
-
-});
 
 
 
